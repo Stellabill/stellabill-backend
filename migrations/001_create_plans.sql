3a8646657722855ff59db93f0b0b060dd612e041
CNduka001 <cnduka.2203652@stu.cu.edu.ng>
2026-04-24 20:53:16 +0100

    feat: complete migration safety checks and policies

--- migrations/001_create_plans.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS plans (
-    id          TEXT PRIMARY KEY,
-    name        TEXT NOT NULL,
-    amount      TEXT NOT NULL,
-    currency    TEXT NOT NULL,
-    interval    TEXT NOT NULL,
-    description TEXT NOT NULL DEFAULT ''
-);

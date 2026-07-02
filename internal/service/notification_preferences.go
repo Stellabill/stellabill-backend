6279eedd3f99a132705b528cb16e6765674998fc
olajide peter tosin <Olajidepeter7012@gmail.com>
2026-07-02 12:39:50 +0100

    Fix: Add missing package declaration to internal/service/notification_preferences.go

--- internal/service/notification_preferences.go
+++ internal/service/notification_preferences.go
@@ -1,3 +1,7 @@
+package service
+
+import "stellarbill-backend/internal/repository"
+
 type NotificationPreferenceService struct {
-    repo repository.NotificationPreferenceRepository
+	repo repository.NotificationPreferenceRepository
 }

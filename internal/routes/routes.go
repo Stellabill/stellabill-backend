c1006047b8f955890c5651fe73832e855f80416b
AvatarMiiDe <olatunleemmanuel@gmail.com>
2026-07-28 16:23:17 +0100

    feat: stream subscription events over WebSocket

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -100,6 +100,7 @@ func Register(r *gin.Engine) {
 	{
 		v1.GET("/subscriptions", h.ListSubscriptions)
 		v1.GET("/subscriptions/:id", handlers.NewGetSubscriptionHandler(svc))
+		v1.GET("/subscriptions/:id/events", h.GetSubscriptionEvents)
 		v1.GET("/plans", h.ListPlans)
 		v1.GET("/statements/:id", handlers.NewGetStatementHandler(stmtSvc))
 		v1.GET("/statements", handlers.NewListStatementsHandler(stmtSvc))

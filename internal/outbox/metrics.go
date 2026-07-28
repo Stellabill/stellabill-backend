2b6b6b9670f3d9e9e9c5ba65b18a1d8c48bc3a04
Ugooweb <ugooweb@gmail.com>
2026-07-28 14:00:09 +0100

    feat: add adaptive concurrency limiter to HTTP publisher

--- internal/outbox/metrics.go
+++ internal/outbox/metrics.go
@@ -4,6 +4,8 @@ import "github.com/prometheus/client_golang/prometheus"
 
 var (
 	OutboxPublisherLag            *prometheus.GaugeVec
+	OutboxPublisherInflight       prometheus.Gauge
+	OutboxPublisherLimit          prometheus.Gauge
 	ChaosOutboxCancellationsTotal prometheus.Counter
 )
 
@@ -16,6 +18,22 @@ func init() {
 		[]string{"publisher"},
 	)
 	_ = prometheus.Register(OutboxPublisherLag)
+
+	OutboxPublisherInflight = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "outbox_publisher_inflight",
+			Help: "Number of in-flight publish requests to the downstream HTTP service",
+		},
+	)
+	_ = prometheus.Register(OutboxPublisherInflight)
+
+	OutboxPublisherLimit = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "outbox_publisher_limit",
+			Help: "Current adaptive concurrency limit for the HTTP publisher",
+		},
+	)
+	_ = prometheus.Register(OutboxPublisherLimit)
 
 	ChaosOutboxCancellationsTotal = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "chaos_outbox_cancellations_total",
